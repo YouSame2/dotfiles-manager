e63e99c065bf97f6968cc851d395eba1646881ea
YouSame2 <businessyousame@gmail.com>
2025-01-01 16:43:00 -0700

    feat: OS detection on wezterm.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -16,14 +16,28 @@ config.window_close_confirmation = 'NeverPrompt'
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
 
+config.front_end = "OpenGL"
+config.max_fps = 144
+config.default_cursor_style = "BlinkingBlock"
+config.animation_fps = 1
+config.cursor_blink_rate = 500
+config.prefer_egl = true
+
 -- -------- WINDOWS SPECIFIC SETTINGS --------
-
--- LEGACY use powershell (for win 11 i think its 'pwsh.exe')
--- config.default_prog = { 'powershell.exe' }
-
--- Use Git Bash
-config.default_prog = { 'C:/Program Files/Git/bin/bash.exe', '--login' }
-
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+    
+    -- LEGACY use powershell (for win 11 i think its 'pwsh.exe')
+    -- config.default_prog = { 'powershell.exe' }
+    
+    -- Use Git Bash
+    config.default_prog = { 'C:/Program Files/Git/bin/bash.exe', '--login' }
+    
+    -- wasnt working for me
+    -- Possible values = Auto | Disable | Acrylic | Mica | Tabbed
+    -- config.win32_system_backdrop = 'Acrylic'
+    
+end
+    
 -- Keep adding configuration options here
 -- 
 
