7697997a6cc633bd560feb543a054bad59de3753
YouSame2 <businessyousame@gmail.com>
2025-01-05 01:32:48 -0700

    add(dotfiles-add.sh): arg processing and partial git functionality

--- dotfiles-add.sh
+++ dotfiles-add.sh
@@ -1,55 +1,169 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 
-# Ensure an argument is passed
-if [ -z "$1" ]; then
-  echo "Error: Please provide a file or directory as an argument."
-  exit 1
+##########################
+# init
+##########################
+
+if [[ -z "$DOTFILES" ]]; then
+    echo "Dotfiles alias is not set please set and rerun script"
+    exit 1
 fi
 
-# Ensure target does not contain subdirs
-if [[ "$1" == */* ]]; then
-  echo "Error: '/' charc found! Subdirectories are not supported yet. To add Target file/directory please first cd to the parent directory of target"
-  exit 1
-fi
 
 # i couldnt find any other way to access my shell functions without sourcing this file. if anyone knows how to allow this subshell script to access .bashrc or .zshrc functions without sourcing let me know!
 source ~/.config/aliases/.universal_aliases
 
-DOTFILES_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-CURRENT_DIR=$(dirs)
+usage() {
+    echo "Usage: $0 {add|link|push|pull} {-m|-w} [-h]"
+    echo "  {add|link|push|pull}           Set the MODE"
+    echo "  -m                             Set OS_FLAG to 'm'"
+    echo "  -w                             Set OS_FLAG to 'w'"
+    echo "  {-h|--h|--help}                Show this help message"
+    exit 1
+}
 
-# TODO change install file 
-INSTALL_FILE="$DOTFILES_DIR/install.conf.yaml"
+MODE=""
+OS_FLAG="u"
+TARGET=""
 
-TARGET=$1
-TARGET_PATH=$(realpath "$TARGET")
-TARGET_NAME=$(basename "$TARGET")
+case "$1" in
+    add|link|push|pull)
+        MODE=$1
+        shift # Remove the first argument after processing. idk this b4
+        ;;
+        -h|--h|--help)
+        usage
+        ;;
+esac
 
-# idk how this works but its something about bash parameter expansion
-RELATIVE_PATH=$(pwd)
-MKDIR_PATH=${RELATIVE_PATH/$HOME/} # mkdir needs this 👈🏽 format
-RELATIVE_PATH=$MKDIR_PATH/$TARGET
-RELATIVE_PATH="${RELATIVE_PATH#/}"
+##########################
+# link functionality
+##########################
 
-# Check if the provided argument is valid
-if [ ! -e "$TARGET_PATH" ]; then
-  echo "Error: '$TARGET_PATH' does not exist."
-  exit 1
+if [ "$MODE" == "link" ]; then
+    echo "link MODE selected" # debug
+    dotfiles-link
+    exit 0
+
+##########################
+# git functionality
+##########################
+
+# TODO make sure this git stuff works. i think i will have to change push to commit and then add git push to it
+elif [[ "$MODE" == "push" ]]; then
+    GIT_ARGS=$@
+    echo "$GIT_ARGS" # debug
+    cd $DOTFILES && git commit . && git $MODE $GIT_ARGS
+    exit 0
+elif [[ "$MODE" == "pull" ]]; then
+    GIT_ARGS=$@
+    echo "$GIT_ARGS" # debug
+    cd $DOTFILES && git $MODE $GIT_ARGS
+    exit 0
+
+##########################
+# add functionality
+##########################
+
+# Parse the arguments
+elif [[ "$MODE" == "add" ]]; then
+    while [[ $# -gt 0 ]]; do
+        case $1 in 
+            -m)
+                OS_FLAG="m"
+                shift
+                ;;
+            -w)
+                OS_FLAG="w"
+                shift
+                ;;
+            -h|--h|--help)
+                usage
+                ;;
+            -*)
+                echo "Unknown option: $1"
+                usage
+                ;;
+            *)
+                if [ -z "$TARGET" ]; then
+                    TARGET=$1
+                else
+                    echo "Error: Multiple files specified. Only one file is allowed for add."
+                    usage
+                fi
+                shift
+                ;;
+        esac
+    done
+
+    echo "Add MODE selected" # debug
+
+    # ensure file provided
+    if [ -z "$TARGET" ]; then
+    echo "Error: Please provide a file or directory as an argument."
+    exit 1
+    fi
+
+    # Ensure file does not contain subdirs
+    if [[ "$TARGET" == */* ]]; then
+    echo "Error: '/' charc found! Subdirectories are not supported yet. To add Target file/directory please first cd to the parent directory of target"
+    exit 1
+    fi
+
+    # DOTFILES_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+    CURRENT_DIR=$(dirs)
+
+    # TODO change install file 
+    INSTALL_FILE="$DOTFILES/install.conf.yaml"
+
+    TARGET_PATH=$(realpath "$TARGET")
+    TARGET_NAME=$(basename "$TARGET")
+
+    # idk how this works but its something about bash parameter expansion
+    RELATIVE_PATH=$(pwd)
+    MKDIR_PATH=${RELATIVE_PATH/$HOME/} # mkdir needs this 👈🏽 format
+    RELATIVE_PATH=$MKDIR_PATH/$TARGET
+    RELATIVE_PATH="${RELATIVE_PATH#/}"
+
+
+    # Check if the provided argument is valid
+    if [ ! -e "$TARGET_PATH" ]; then
+    echo "Error: '$TARGET_PATH' does not exist."
+    exit 1
+    fi
+
+    # Make and move the target to the dotfiles directory
+    mkdir -p "$DOTFILES/$MKDIR_PATH" && \
+    mv -i "$TARGET" "$DOTFILES/$MKDIR_PATH" && \
+    # create symlink path
+    yq -i "(.[] | select(.link) | .[].\"$CURRENT_DIR/$TARGET_NAME\") = \"$RELATIVE_PATH\"" "$INSTALL_FILE" || \
+    { echo "Error: Please double check the files and OS compatibility."; exit 1; }
+    # 👆🏼 yq = confusing af...
+
+    # Confirmation
+    echo ''
+    echo "'$TARGET' has been added to 'install.conf.yaml' and moved to '$DOTFILES/$MKDIR_PATH'."
+
+    # Create symlinks
+    dotfiles-link
+    exit 0
 fi
 
-# Make and move the target to the dotfiles directory
-mkdir -p "$DOTFILES_DIR/$MKDIR_PATH" && \
-mv -i "$TARGET" "$DOTFILES_DIR/$MKDIR_PATH" && \
-# create symlink path
-yq -i "(.[] | select(.link) | .[].\"$CURRENT_DIR/$TARGET_NAME\") = \"$RELATIVE_PATH\"" "$INSTALL_FILE" || \
-{ echo "Error: Please double check the files and OS compatibility."; exit 1; }
-# 👆🏼 yq = confusing af...
 
-# Confirmation
-echo ''
-echo "'$TARGET' has been added to 'install.conf.yaml' and moved to '$DOTFILES_DIR/$MKDIR_PATH'."
 
-# Create symlinks
-dotfiles-sync
+
+
+
+
+
+
+
+# if [ "$OS_FLAG" == "m" ]; then
+#     echo "Operating system set to 'm'" # debug
+#     # Logic for OS_FLAG 'm' here
+# elif [ "$OS_FLAG" == "w" ]; then
+#     echo "Operating system set to 'w'" # debug
+#     # Logic for OS_FLAG 'w' here
+# fi
