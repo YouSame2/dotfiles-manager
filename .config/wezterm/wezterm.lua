a0599d68b63b39987d5d180d69ea4f0771f4f212
YouSame2 <businessyousame@gmail.com>
2025-01-13 00:06:35 -0700

    YEET dotfiles

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -16,7 +16,7 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_close_confirmation = 'NeverPrompt'
 config.adjust_window_size_when_changing_font_size = false
 
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.95
 config.macos_window_background_blur = 10
 
 config.front_end = "WebGpu" -- breaks transparency on win
@@ -25,6 +25,16 @@ config.max_fps = 60
 config.default_cursor_style = "BlinkingBlock"
 config.animation_fps = 60
 config.cursor_blink_rate = 500
+
+---------- KEYBINDINGS --------
+config.keys = {
+    -- Default QuickSelect keybind (CTRL-SHIFT-Space) gets captured by something else on my system
+    {
+      key = 'A',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.QuickSelect,
+    },
+  }
 
 ---------- WINDOWS SPECIFIC SETTINGS --------
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
@@ -45,7 +55,6 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
 end
 return config
 
-
--- OTHER GOOD THEMES:
--- Tokyo Night
--- Catppuccin Macchiato (Gogh)
+---------- CREDIT AND RESOURCES --------
+-- https://github.com/mplusp/dotfiles/tree/main
+-- OTHER GOOD THEMES: Tokyo Night, Catppuccin Macchiato (Gogh)
