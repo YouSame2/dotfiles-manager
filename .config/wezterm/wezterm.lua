d333492f781c4f9a34a10eee5097aa63d3251125
YouSame2 <businessyousame@gmail.com>
2025-01-16 16:08:27 -0700

    CNS takeover

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,8 +4,6 @@ local wezterm = require("wezterm")
 -- This will hold the configuration
 local config = wezterm.config_builder()
 
-wezterm.gui.enumerate_gpus()
-
 config.font = wezterm.font("Hack Nerd Font Mono")
 config.font_size = 14
 
